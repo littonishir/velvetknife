a56099e92aa878a551acb14f91b9d2e7bb63dc65
littonishir <littonishir@gmail.com>
2018-09-14 10:58:09 +0800

    完善文档

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# velvetknife
-copy write butterknife
+# velvetknif
+
+## 天鹅绒刀
+
+ViewBind ：取代findViewById
+
+OnClick ： 设置点击事件
+
+CheckNet ：检查网路是否可以用
+
+## 使用
+
+```java
+public class MainActivity extends AppCompatActivity {
+    @ViewBind(R.id.tv)
+    TextView textView;
+    @ViewBind(R.id.tv1)
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VelvetKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv, R.id.tv1})
+    @CheckNet(netErrMsg = "请检查网路", showToast = true)
+    public void Onclick(View view) {
+        switch (view.getId()) {
+            case R.id.tv:
+                textView.setText("网络通信正常");
+                break;
+            case R.id.tv1:
+                textView1.setText("그녀를 보면");
+                break;
+        }
+    }
+}
+```
+
+## 代码持续更新
+
+需要新功能 欢迎提issue 
+
--- app/src/main/java/com/littonishir/velvetknife/MainActivity.java
+++ app/src/main/java/com/littonishir/velvetknife/MainActivity.java
@@ -1,7 +1,8 @@
 package com.littonishir.velvetknife;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.littonishir.velvetknife.annotation.CheckNet;
@@ -11,15 +12,26 @@ import com.littonishir.velvetknife.annotation.ViewBind;
 public class MainActivity extends AppCompatActivity {
     @ViewBind(R.id.tv)
     TextView textView;
+    @ViewBind(R.id.tv1)
+    TextView textView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         VelvetKnife.bind(this);
     }
-    @OnClick(R.id.tv)
-    @CheckNet(netErrMsg = "请检查网路",showToast = true)
-    public void test(){
-        textView.setText("网络通信正常");
+
+    @OnClick({R.id.tv, R.id.tv1})
+    @CheckNet(netErrMsg = "请检查网路", showToast = true)
+    public void Onclick(View view) {
+        switch (view.getId()) {
+            case R.id.tv:
+                textView.setText("网络通信正常");
+                break;
+            case R.id.tv1:
+                textView1.setText("그녀를 보면");
+                break;
+        }
     }
 }
